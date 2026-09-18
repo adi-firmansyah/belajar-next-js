76df1eede0d00277c3bca6e1c2993b302d9bdb09
Adi Firmansyah <adif4659@gmail.com>
2026-09-18 09:26:49 +0700

    feat: menambahkan komponen toaster pada root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "@/app/globals.css";
+import { Toaster } from "@/components/ui/toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">
+        <main>{children}</main>
+        <Toaster />
+      </body>
     </html>
   );
 }
